38aed6c405d24f5a0cd77b6be2c03119f434648c
Ben Zeghers <bzeghers@gmail.com>
2014-12-03 10:59:17 -0600

    Improve output format

--- find_common.py
+++ find_common.py
@@ -15,10 +15,11 @@ win_sets, win_support = ap.apriori(win_teams, min_support)
 all_sets, all_support = ap.apriori(all_teams, min_support)
 
 for i in range(len(win_sets)):
-    counter = Counter({heroes: win_support[heroes] / float(all_support[heroes]) for heroes in win_sets[i]})
-    
+    counter = Counter({heroes: win_support[heroes] / (float(all_support[heroes])*2) * 100 for heroes in win_sets[i]})
+
     for hero_set in counter.most_common(10):
-        print ", ".join([HEROES[h] for h in [h for h in iter(hero_set[0])]]), win_support[hero_set[0]], "/",
-                all_support[hero_set[0]], "=", round(hero_set[1], 4)
-    
+        hero_list = [HEROES[h] for h in [h for h in iter(hero_set[0])]]
+
+        print ("%-" + str(len(hero_list)*15) + "s%.4f%%") % (", ".join(hero_list), hero_set[1])
+
     print
