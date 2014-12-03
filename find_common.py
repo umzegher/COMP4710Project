1a23f501c5a53e9eda4f396ed0059b63caa19f7a
Ben Zeghers <bzeghers@gmail.com>
2014-12-03 00:10:15 -0600

    Update min_support value

--- find_common.py
+++ find_common.py
@@ -4,7 +4,7 @@ from collections import Counter
 import apriorialg as ap
 from heroes import HEROES
 
-min_support = 0.01
+min_support = 0.001
 
 matches = [ast.literal_eval(data) for data in open("dota2_details.txt", 'r').readlines()]
 
@@ -12,12 +12,13 @@ win_teams = [match['win'] for match in matches]
 all_teams = win_teams + [match['lose'] for match in matches]
 
 win_sets, win_support = ap.apriori(win_teams, min_support)
-all_sets, all_support = ap.apriori(all_teams, min_support/10)
+all_sets, all_support = ap.apriori(all_teams, min_support)
 
 for i in range(len(win_sets)):
     counter = Counter({heroes: win_support[heroes] / float(all_support[heroes]) for heroes in win_sets[i]})
     
     for hero_set in counter.most_common(10):
-        print ", ".join([HEROES[h] for h in [h for h in iter(hero_set[0])]]), win_support[hero_set[0]], "/", all_support[hero_set[0]], "=", round(hero_set[1], 4)
+        print ", ".join([HEROES[h] for h in [h for h in iter(hero_set[0])]]), win_support[hero_set[0]], "/",
+                all_support[hero_set[0]], "=", round(hero_set[1], 4)
     
     print
